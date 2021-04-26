e2100eec098850b7d7b1d22331b76a12a22aa332
Bill Adams <badams.prog@protonmail.com>
2021-04-26 17:11:04 -0500

    Adding CLI and its tests
    infrastructure for build and test runner/cloverage

---
+++ src/badams/cli.clj
@@ -0,0 +1,40 @@
+(ns badams.cli
+  (:gen-class)
+  (:require [badams.core :as core]
+            [badams.repository :as repo]
+            [clojure.java.io :as io]
+            [clojure.pprint :as pp]
+            [clojure.string :as str]
+            [clojure.walk :as walk]))
+
+(defn options [args]
+  (into {} (map vec (partition 2 args))))
+
+(defn create-repo! [resource-path]
+  (with-open [rdr (io/reader resource-path)]
+    (let [repo (repo/atomic-repo)]
+      (doseq [line (line-seq rdr)]
+        (some->> (core/parse line)
+                 (repo/put!  repo)))
+      repo)))
+
+(def report-views
+  {repo/by-email-desc-last-name-asc "By email descending, last name ascending"
+   repo/by-birth-date-asc           "By birth date ascending"
+   repo/by-last-name-desc           "By last name descending"})
+
+(defn ^:private show-table! [title coll]
+  (println (str/join "" (repeat (count title) " ")))
+  (printf  "\n|\t%s:\n|" title)
+  (pp/print-table (walk/stringify-keys coll)))
+
+(defn show-report! [repo [order title :as view]]
+  (->> (repo/values repo {:order order})
+       (map core/prepare)
+       (show-table! title)))
+
+(defn -main [& args]
+  (let [file (get (options args)  "-f")
+        repo (create-repo! file)]
+    (doseq [v report-views]
+      (show-report! repo v))))
---
+++ test/badams/cli_test.clj
@@ -0,0 +1,70 @@
+(ns badams.cli-test
+  (:require [badams.cli :as sut]
+            [clojure.test :refer :all]
+            [badams.repository :as repo]
+            [badams.core :as core]
+            [clojure.string :as str]))
+
+(deftest show-report!-test
+  (let [repo (repo/atomic-repo [{:line 1 :DateOfBirth (core/date "1/2/1970")}
+                                {:line 2 :DateOfBirth (core/date "1/3/1980")}])]
+
+    (is (=
+"|	TEST:
+|
+| line | DateOfBirth |
+|------+-------------|
+|    1 |    1/2/1970 |
+|    2 |    1/3/1980 |"
+
+(str/trim (with-out-str (sut/show-report! repo [repo/unsorted "TEST"])))))))
+
+(deftest create-repo!-test
+  (is (= [{:FirstName "FN"
+           :LastName "LN"
+           :Email "EM"
+           :FavoriteColor "FC"
+           :DateOfBirth (core/date "1/1/1970")}]
+        (repo/values (sut/create-repo! "./test/resources/cli_test.csv")))))
+
+(deftest -main-test
+  (let [results
+        (str/trim
+          (with-out-str
+            (sut/-main "-f" "./test/resources/cli_int_test.csv")))]
+    (is (=
+"|\tBy email descending, last name ascending:
+|
+| LastName | FirstName | Email | FavoriteColor | DateOfBirth |
+|----------+-----------+-------+---------------+-------------|
+|       LN |        FN |   EM4 |            FC |    1/3/1990 |
+|      LNB |        FN |   EM4 |            FC |    1/3/1990 |
+|      LND |        FN |   EM4 |            FC |    1/3/1990 |
+|      LNB |        FN |   EM3 |            FC |    1/1/1980 |
+|      LNC |        FN |   EM2 |            FC |    1/5/2000 |
+|      LN0 |        FN |   EM1 |            FC |    1/1/1960 |
+                       
+
+|\tBy birth date ascending:
+|
+| LastName | FirstName | Email | FavoriteColor | DateOfBirth |
+|----------+-----------+-------+---------------+-------------|
+|      LN0 |        FN |   EM1 |            FC |    1/1/1960 |
+|      LNB |        FN |   EM3 |            FC |    1/1/1980 |
+|      LNB |        FN |   EM4 |            FC |    1/3/1990 |
+|       LN |        FN |   EM4 |            FC |    1/3/1990 |
+|      LND |        FN |   EM4 |            FC |    1/3/1990 |
+|      LNC |        FN |   EM2 |            FC |    1/5/2000 |
+                       
+
+|\tBy last name descending:
+|
+| LastName | FirstName | Email | FavoriteColor | DateOfBirth |
+|----------+-----------+-------+---------------+-------------|
+|      LND |        FN |   EM4 |            FC |    1/3/1990 |
+|      LNC |        FN |   EM2 |            FC |    1/5/2000 |
+|      LNB |        FN |   EM3 |            FC |    1/1/1980 |
+|      LNB |        FN |   EM4 |            FC |    1/3/1990 |
+|      LN0 |        FN |   EM1 |            FC |    1/1/1960 |
+|       LN |        FN |   EM4 |            FC |    1/3/1990 |"
+           results))))
